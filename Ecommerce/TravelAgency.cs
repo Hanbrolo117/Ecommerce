788941f68987f215296ae39468d9a43a2eaef588
Khal <Khal@DESKTOP-OO651L1>
2016-09-24 01:31:44 -0700

    TravelAgency2

--- Ecommerce/TravelAgency.cs
+++ Ecommerce/TravelAgency.cs
@@ -9,27 +9,42 @@ namespace Ecommerce
     class TravelAgency
     {
         int agency_id; //Variable N where N = 5 (TravelAgency1 --> TravelAgency5)
+        int credit_card;
 
-        public TravelAgency (int agency_id)
+        public TravelAgency (int agency_id, int credit_card)
         {
             this.agency_id = agency_id;
 
-            //TODO: Apply for credit card number
+
+            this.credit_card = credit_card;
+           
             //TODO: OrderProcessing
         }
+
+        public void setCCNumber(int ccNumber)
+        {
+            credit_card = ccNumber;
+        }
+
         public void hotelPriceBeenCut(decimal current_price, decimal new_price, string id)
         {
             int rooms_to_order;
-            bool demand = false; //If there is high demand, demand = TRUE
+            int demand = 0; //Demand is a variable that will be used as a multiplier for how many rooms to order
 
-            if (Hotel.current_number_of_rooms_available < 70)
+            if (availablerooms < (Hotel.MAX_ROOMS*.20))
             { //If current availability is less than 20% of max rooms
-                demand = true;
+                demand++; //Increase demand
+            } else
+
+            if(current_price < new_price)
+            {
+                demand--; //If the new price is higher than the old price, decrease demand
             } else
             {
-                demand = false;
+                demand++; //If the new price is lower than the older price, increase demand
             }
 
+            rooms_to_order = (availablerooms - (10 * demand)); //Take whatever rooms are available, subtract it by 10 * demand
         }
 
         private void placeOrder(OrderObject order)
@@ -39,6 +54,10 @@ namespace Ecommerce
             //TODO : create thread to place order?
         }
 
-        private decimal 
+        private void confirmOrder(OrderObject orderConfirm)
+        {
+            string timeStamp = DateTime.Now.ToString(); //Create a time stamp for when order is confirmed
+            Console.WriteLine(orderConfirm);
+        }
     }
 }   
