c0b14e3df552a8d1c15dd7c817a1e5ae6311bced
hehe876 <ndao3@asu.edu>
2016-09-24 01:37:39 -0700

    First complete version of MultiCellBuffer

--- Ecommerce/MultiCellBuffer.cs
+++ Ecommerce/MultiCellBuffer.cs
@@ -22,50 +22,101 @@ namespace Ecommerce
         public class Cell
         {
             string encodedStr;
-            int id;
 
             public Cell()
             {
                 this.encodedStr = "";
-                this.id = -1;
             }
 
-            public string getString()
+            public string getEncodedString()
             {
                 return this.encodedStr;
             }
 
-            public int getID()
-            {
-                return this.id;
-            }
-
-            public void setCell(string message)
+            public void setEncodedString(string message)
             {
                 this.encodedStr = message;
             }
-
-            public void setID(int idnum)
-            {
-                this.id = idnum;
-            }
-
         }
+
+        /**
+         * MultiCellBuffer
+         * Fields:
+         *  +Semaphore pool: keep track of available cell to be read or written
+         *  +cellArray: array of 3 cells
+         * Fucntions:
+         *  +void setOneCell(string encodedMessage): set cell's encoded message
+         *  +string getOneCell(): get cell cencoded message
+         */
         class MultiCellBuffer
         {
             Semaphore pool = new Semaphore(0, 3);
-            Cell[] cellArray;
+            Cell[] cellArray = new Cell[3];
 
+            //Contructor - Initializing cells
             public MultiCellBuffer()
             {
                 for (int i = 0; i < 3; i++)
                     this.cellArray[i] = new Cell();
             }
 
-            //Cell getOneCell()   { return Cell someCell; }
-            //void setOneCell() { cellArray[someCell].encodedStr = someString;}
+            /*
+             * If a semaphore indicate free resorce, setOneCell will TryEnter each cell
+             * until it sees a free cell and will lock that cell until it finish writting
+             */        
+            void setOneCell(string encodedMessage)
+            {
+                //wait for a resource to be free
+                pool.WaitOne();
+
+                //once there is a resource freeing up
+                //try enter each cell to find an available cell
+                for (int i = 0; i < 3; i++)
+                {
+                    if (Monitor.TryEnter(cellArray[i]))          //if a cell in locked, move on to the next one
+                    {
+                        cellArray[i].setEncodedString(encodedMessage);
+                        Monitor.Exit(cellArray[i]);          //unlock the object after writing
+                        pool.Release(1);
+                        i = 4;
+                    }
+                }
+            }
+
+            /*
+             * If a semaphore indicate free resorce, setOneCell will TryEnter each cell
+             * until it sees a free cell and will lock that cell until it finish reading
+             * returns empty string if cell is free but doesn't have value in it
+             */
+            string getOneCell()
+            {
+                string result = "";
+                //wait for a resource to be free
+                pool.WaitOne();
+
+                //once there is a resource freeing up
+                //try enter each cell to find an available cell
+                for (int i = 0; i < 3; i++)
+                {
+                    if (Monitor.TryEnter(cellArray[i]))
+                    {
+                        //if the cell have an order in it
+                        if (cellArray[i].getEncodedString().Length != 0)
+                        {
+                            result = cellArray[i].getEncodedString();
+                            cellArray[i].setEncodedString("");
+                            pool.Release(1);
+                            Monitor.Exit(cellArray[i]);
+                        }
+                        else
+                        {
+                            Monitor.Exit(cellArray[i]);
+                            pool.Release(1);
+                        }
+                    }
+                }
+                return result;
+            }
         }
-
     }
-
 }
