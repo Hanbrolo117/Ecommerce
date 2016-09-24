627996c3b339da146c29fc6c2b962702e948b601
Kyle Ferguson <hanbrolo117@gmail.com>
2016-09-23 23:00:10 -0700

    delegate in Hotel added

--- Ecommerce/Hotel.cs
+++ Ecommerce/Hotel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ecommerce
@@ -9,11 +10,22 @@ namespace Ecommerce
     class Hotel
     {
 
+        public delegate decimal PriceCut(decimal previous_price, decimal current_price);
 
+        public void HotelFunc() {
+            while (1) {
+                Thread.Sleep(500);
+                //TODO::UPDATE PRICE MODEL
+
+                //TODO::CHECK t
+            }
+
+
+
+        }
 
         private class PriceModel {
 
         }
-
     }
 }
--- Ecommerce/MultiCellBuffer.cs
+++ Ecommerce/MultiCellBuffer.cs
@@ -8,5 +8,6 @@ namespace Ecommerce
 {
     class MultiCellBuffer
     {
+
     }
 }
