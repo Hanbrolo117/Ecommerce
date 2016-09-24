9bcf20131bb0cf4fadf3db9f2964164387c9799e
hehe876 <ndao3@asu.edu>
2016-09-23 23:51:04 -0700

    MultiCellBuffer Data Strucure

--- Ecommerce/MultiCellBuffer.cs
+++ Ecommerce/MultiCellBuffer.cs
@@ -6,8 +6,54 @@ using System.Threading.Tasks;
 
 namespace Ecommerce
 {
-    class MultiCellBuffer
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+
+    namespace Ecommerce
     {
+        public class Cell
+        {
+            string encodedStr;
+            int id;
+
+            public Cell()
+            {
+                this.encodedStr = "";
+            }
+
+            public string getString()
+            {
+                return this.encodedStr;
+            }
+
+            public int getID()
+            {
+                return this.id;
+            }
+
+            public void setCell(string message)
+            {
+                this.encodedStr = message;
+            }
+        }
+        class MultiCellBuffer
+        {
+            Semaphore pool = new Semaphore(0, 3);
+            Cell[] cellArray;
+
+            public MultiCellBuffer()
+            {
+                for (int i = 0; i < 3; i++)
+                    this.cellArray[i] = new Cell();
+            }
+
+            //Cell getOneCell() { return Cell someCell; }
+            //void setOneCell() { cellArray[someCell].encodedStr = someString;}
+        }
 
     }
+
 }
